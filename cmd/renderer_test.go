bfd6467e9ee9d819a2d381a37f87a53fc3b7731d
ryou <ryo@example.com>
2024-07-27 02:17:17 +0900

    add createImage

--- cmd/renderer_test.go
+++ cmd/renderer_test.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -11,5 +13,22 @@ func TestCreateFormat(t *testing.T) {
 	if format != expected {
 		t.Errorf("expected: %v", expected)
 		t.Errorf("result: %v", format)
+	}
+}
+
+func TestCreateImage(t *testing.T) {
+	image := createImage(256, 256)
+
+	for _, row := range strings.Split(image, "\n") {
+		for _, pixel := range strings.Fields(row) {
+			color, err := strconv.Atoi(pixel)
+			if err != nil {
+				t.Error(err)
+			}
+			if color < 0 || color > 255 {
+				t.Errorf("Color is greater than or equal to 0 and less than or equal to 255.")
+				t.Errorf("result: %d", color)
+			}
+		}
 	}
 }
