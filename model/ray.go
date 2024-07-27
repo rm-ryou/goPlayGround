305f75dbf1f64fd7b1a633dd1f3ac13004016007
rm-ryou <emco.420118@gmail.com>
2024-07-27 19:50:39 +0900

    fix caluc color when ray colision to sphere

--- model/ray.go
+++ model/ray.go
@@ -1,6 +1,8 @@
 package model
 
-import "math"
+import (
+	"math"
+)
 
 type Ray struct {
 	Orig Vec3D
@@ -11,24 +13,10 @@ func (r Ray) At(t float64) Vec3D {
 	return r.Orig.Add(r.Dir.MultiNum(t))
 }
 
-func (r Ray) isHitSphere(center Vec3D, radius float64) float64 {
-	oc := center.Sub(r.Orig)
-	a := r.Dir.Dot(r.Dir)
-	b := -2 * r.Dir.Dot(oc)
-	c := oc.Dot(oc) - radius*radius
-	discriminant := b*b - 4*a*c
-
-	if discriminant < 0 {
-		return math.NaN()
-	} else {
-		return (-b - math.Sqrt(discriminant)) / (2 * a)
-	}
-}
-func (r Ray) Color() Color {
-	t := r.isHitSphere(Vec3D{0, 0, -1}, 0.5)
-	if t > 0 {
-		n := r.At(t).Sub(Vec3D{0, 0, -1}).Norm()
-		return Vec3D{n.X + 1, n.Y + 1, n.Z + 1}.MultiNum(0.5).Vec3DToColor()
+func (r Ray) Color(worldObjects HittableList) Color {
+	hitRec := new(HitRecord)
+	if worldObjects.Hit(r, math.Inf(1), 0, hitRec) {
+		return Vec3D{1, 1, 1}.Add(hitRec.Norm).MultiNum(0.5).Vec3DToColor()
 	}
 
 	unitDir := r.Dir.Norm()
